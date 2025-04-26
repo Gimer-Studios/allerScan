baaca051fbb7dbaeb5b054b97a872da56b792848
Gimer-Studios <gimerstudios@gmail.com>
2025-04-26 15:31:24 -0400

    Changes to be committed: 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#ffffff',
-        base: "/allerScan",
+        base: "/allerScan/",
         icons: [
           {
             src: 'pwa-192x192.png',
